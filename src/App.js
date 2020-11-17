11952f1dd8f4682dd507c0f84b31a1c4bba92a24
mozumderTushar <itztushar55@gmail.com>
2020-11-17 13:15:12 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Layout from './AdminComponent/Layout/Layout';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { createContext, useState } from 'react';
+import Login from './Login/Login/Login';
+
+export const UserContext = createContext();
 
 function App() {
+	const [loggedInUser, setLoggedInUser] = useState({})
 	return (
-		<div className="">
+		<UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
 			<BrowserRouter>
 				<Switch>
 
 					<Route path='/admin'><Layout /> </Route>
 
+					<Route path='/login'><Login /> </Route>
+
 					<Route path='*'> <h4> 404 error </h4> </Route>
 
 				</Switch>
 			</BrowserRouter>
-		</div>
+		 </UserContext.Provider>
 	);
 }
 
